c5202de16d246b3389196a2c428222499d9c8a23
G00425238@atu.ie <g00425238@atu,ie>
2024-10-08 11:01:20 +0100

    Get and Add Product features

---
+++ src/main/java/ie/atu/week4cicd1/ProductController.java
@@ -0,0 +1,31 @@
+package ie.atu.week4cicd1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+    private List<Product> productList = new ArrayList<>();
+
+    public ProductController(){
+        productList.add(new Product("100", "Tv", "Electric", 399));
+        productList.add(new Product("101", "Radio", "Eelctric", 399));
+    }
+
+    @GetMapping("/getProducts")
+    public List<Product> getProduct(){
+        return productList;
+    }
+
+    @PostMapping("addProduct")
+        public ResponseEntity<List> addProduct(@RequestBody Product product){
+        productList.add(product);
+        return ResponseEntity.ok(productList);
+    }
+
+
+}
